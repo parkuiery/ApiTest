d5bd93ac8a632c8aa8ad2f2d3c613188253dd4c9
parkUiery <parkuiery@gmail.com>
2023-06-14 17:38:32 +0900

    perf :: WeatherSysModeal

--- ApiPractice/app/src/main/java/com/example/apipractice/WeatherSysModeal.java
+++ ApiPractice/app/src/main/java/com/example/apipractice/WeatherSysModeal.java
@@ -1,2 +1,17 @@
-package com.example.apipractice;public class WeatherSysModeal {
+package com.example.apipractice;
+
+import com.google.gson.annotations.SerializedName;
+
+public class WeatherSysModeal {
+
+    @SerializedName("country")
+    String country = ""; //나라
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country){
+        this.country = country;
+    }
 }
