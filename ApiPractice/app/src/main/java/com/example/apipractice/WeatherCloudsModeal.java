e82032d00d11cdd00308e4fdcd159ee1a6e4324d
parkUiery <parkuiery@gmail.com>
2023-06-14 17:38:18 +0900

    perf :: WeatherCloudsModeal

--- ApiPractice/app/src/main/java/com/example/apipractice/WeatherCloudsModeal.java
+++ ApiPractice/app/src/main/java/com/example/apipractice/WeatherCloudsModeal.java
@@ -1,2 +1,17 @@
-package com.example.apipractice;public class WeatherCloudsModeal {
+package com.example.apipractice;
+
+import com.google.gson.annotations.SerializedName;
+
+public class WeatherCloudsModeal {
+
+    @SerializedName("all")
+    double all = 0.0; //구름
+
+    public double getAll() {
+        return all;
+    }
+
+    public void setAll(double all) {
+        this.all = all;
+    }
 }
