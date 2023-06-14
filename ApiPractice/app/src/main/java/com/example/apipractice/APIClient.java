76dff1a62a2970e842515eab9a1e7d07f02e6aee
parkUiery <parkuiery@gmail.com>
2023-06-14 17:37:33 +0900

    perf :: APIClient

--- ApiPractice/app/src/main/java/com/example/apipractice/APIClient.java
+++ ApiPractice/app/src/main/java/com/example/apipractice/APIClient.java
@@ -1,2 +1,27 @@
-package com.example.apipractice;public class APIClient {
+package com.example.apipractice;
+
+import android.util.Log;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class APIClient {
+    private static final String TAG = APIClient.class.getSimpleName();
+
+    public static Retrofit getClient(String url) {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+        Log.d(TAG, "APIClient >> url : "+url);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+        return retrofit;
+    }
 }
