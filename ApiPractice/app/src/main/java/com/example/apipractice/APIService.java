6e36cf529b4c582880c54f4a4bedbadcadecd4af
parkUiery <parkuiery@gmail.com>
2023-06-15 23:29:29 +0900

    refactor :: 코드 수정

--- ApiPractice/app/src/main/java/com/example/apipractice/APIService.java
+++ ApiPractice/app/src/main/java/com/example/apipractice/APIService.java
@@ -14,4 +14,11 @@ public interface APIService {
             @Query("q") String q,
             @Query("appid") String appid
     );
+
+    @GET("data/2.5/weather")
+    Call<WeatherInfoModeal> fetchWeatherInfo(
+            @Query("lat") Double lat,
+            @Query("lon") Double lon,
+            @Query("appid") String appid
+    );
 }
